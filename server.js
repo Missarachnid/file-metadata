7bb6b903c44e3d6d69dbc1c2ea732d95579ab5e0
Glitch (kepler) <none>
2018-04-05 22:56:58 +0000

    🖐🗺 Checkpoint ./package.json:503759/325 ./server.js:503759/403 ./public/style.css:503759/1290

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('views'));
+app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function (req, res) {
+ 
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
